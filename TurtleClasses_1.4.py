c66d073e002d738162e42031897e3e6a48ef61bf
Ryan <ryanscottmckay@gmail.com>
2019-06-29 17:07:53 -0600

    Added a bunch of pattern functions, many of which aren't finished/ functional

--- TurtleClasses_1.4.py
+++ TurtleClasses_1.4.py
@@ -66,6 +66,7 @@ class Turt:
         self._speed = speed
         self._heading = heading
         self.turtle = turtle.Turtle()
+        self.turtle.color('magenta')
         if isinstance(self._color, str):
             self.turtle.color(self._color)
         self.turtle.pensize(self._size)
@@ -73,8 +74,8 @@ class Turt:
         self.turtle.penup()
         self.turtle.goto(xy[0], xy[1])
         self.turtle.seth(self._heading)
-        self.turtle.pendown()
         self._xy = self.turtle.pos()
+        self.turtle.pendown()
 
     @property
     def color(self):
@@ -99,7 +100,7 @@ class Turt:
 
     @color.setter
     def color(self, col):
-        print("color setter method called")
+        # print("color setter method called")
         self._color = col
         self.turtle.color(self._color)
 
@@ -149,8 +150,49 @@ class Pattern:
         self._reps = repeats
         self._colors = colist
 
+    def pen(self, UoD):
+        if UoD == 0 or UoD == 'up':
+            self._turt.turtle.penup()
+        elif UoD == 1 or UoD == 'down':
+            self._turt.turtle.pendown()
+
+    def color(self, color):
+        self._turt.color = color
+
+    def left(self, angle):
+        self._turt.turtle.left(angle)
+
+    def right(self, angle):
+        self._turt.turtle.right(angle)
+
     def forward(self, dist):
         self._turt.turtle.forward(dist)
+
+    def curve(self, size, range_, fwd, angle, color=None):
+        if color != None:
+            self._turt.turtle.color(color)
+        for i in range(range_):
+            self.forward(fwd * size)
+            self.left(angle)
+
+    def fadecurve(self, size, range_, fwd, angle, color=None):
+        if color != None:
+            self._turt.turtle.color(color)
+        for i in range(range_):
+            self.forward(fwd * size)
+            self.left(angle)
+
+    def petal(self, size, range_, fwd, angle):
+        self.curve(size, range_, fwd, angle)
+        self.left(90)
+        self.curve(size, range_, fwd, angle)
+        self.left(90)
+
+    def fadepetal(self, size, range_, fwd, angle):
+        self.curve(size, range_, fwd, angle)
+        self.left(90)
+        self.curve(size, range_, fwd, angle)
+        self.left(90)
 
     def circle(self, radius, extent=None, steps=None):
         self._turt.turtle.circle(radius, extent=extent, steps=steps)
@@ -235,10 +277,143 @@ class Pattern:
             for i in range(yrange_):
                 self.rainbowcircles_centered(repeats, radius)
 
+    def flower1(self, npetals=6, size=100, color='blue'):  # NOT DONE
+        self.pen('down')
+        self.color(color)
+        for i in range(100):
+            for j in range(15):
+                self.forward(10)
+                self.right(4.5)
+            self.right(100)
+            for j in range(15):
+                self.forward(10)
+                self.right(4.5)
+            self.left(100)
+        # for i in range(npetals):
+        #     self.circle(size, extent=80)
+        #     self.left(40)
+        #     self.circle(size, extent=80)
+        #     self.right(140)
 
-speed = 1000
+    def flower2(self, npetals=6, size=100, color='blue'):  # NOT DONE
+        self.pen('down')
+        self.color(color)
+        for i in range(100):
+            # self.circle(size, extent=80)
+            self.left(20)
+            for j in range(5):
+                self.forward(50)
+                self.right(20)
+            # self.circle(size, extent=80)
+            self.right(160)
+
+    def flower3(self, npetals=6, size=100, color='blue'):  # NOT DONE
+        self.pen('down')
+        self.color(color)
+        for i in range(100):
+            # self.left(20)
+            for j in range(5):
+                self.forward(50)
+                self.right(9)
+            self.right(120)
+
+    def loopy_flower(self, npetals=7, size=100, loopsize=50, roundness=2, color='blue'):
+        # self.pen('down')
+        # self.color(color)
+        angle = (1800 / npetals) / loopsize
+        for i in range(100):
+            self.forward(size)
+            for j in range(loopsize):
+                self.right(angle)
+                self.forward(roundness)
+            # self.left(angle)
+
+    def loopy_square(self, npetals=8, size=100, roundness=2, color='blue'):   # NOT DONE
+        # self.pen('down')
+        # self.color(color)
+        angle = 1800 / npetals
+        for i in range(100):
+            self.forward(size)
+            for j in range(27):
+                self.right(10)
+                self.forward(roundness)
+            # self.left(angle)
+
+    def spiral(self, speed=5, color='blue'):        # NOT DONE
+        self.color(color)
+        self.pen('down')
+        x = 5
+        y = 1
+        for i in range(100):
+            self.forward(y)
+            self.left(x)
+            x += 0.2
+            y += 2
+
+    def flowerp(self, npetals=5, size=1, curverange=10, petalfwd=15, curveangle=9, colors=None, pos=[0, 0]):
+        # radius = petalfwd / curverange
+        if colors == None:
+            colors = self._turt.color
+        self.pen('up')
+        self._turt.turtle.goto(pos[0], pos[1])
+        self.pen('down')
+        for i in range(npetals + 1):
+            col = colors[i % len(colors)]
+            if isinstance(colors, str):
+                col = colors
+            self._turt.color = col
+            self.petal(size, curverange, petalfwd, curveangle)
+            self.right(360 / npetals)
+
+    def many_flowers(self, petals=24, size=1, curverange=10, petalfwd=15, curveangle=9, colors=None, pos=[0, 0], rotation=4.5):  # NOT DONE
+        if colors is None:
+            colors = self._turt.color
+        radius = petalfwd * curveangle
+        print('radius: ' + str(radius))
+        xint = radius
+        yint = 750
+        xdiv = 2000 / radius
+        xlist = [-1000 + (i * radius) for i in range(round(xdiv) + 1)]
+        yrange_ = int((2*yint) / xint)
+        self._turt.heading = 4.5
+        for j in xlist:
+            self.flowerp(petals, size, curverange, pos=[j, yint], colors=colors)
+            curpos = self._turt.turtle.pos()
+            for i in range(yrange_):
+                self.flowerp(petals, size, curverange, pos=[curpos[0], curpos[1] - radius], colors=colors)
+                curpos = self._turt.turtle.pos()
+
+    def many_fadepetal_flowers(self, petals=24, size=1, curverange=10, petalfwd=15, curveangle=9, colors=None, pos=[0, 0], rotation=4.5):  # NOT DONE
+        if colors is None:
+            colors = self._turt.color
+        radius = petalfwd * curveangle
+        print('radius: ' + str(radius))
+        xint = radius
+        yint = 750
+        xdiv = 2000 / radius
+        xlist = [-1000 + (i * radius) for i in range(round(xdiv) + 1)]
+        yrange_ = int((2*yint) / xint)
+        self._turt.heading = 4.5
+        for j in xlist:
+            self.flowerp(petals, size, curverange, pos=[j, yint], colors=colors)
+            curpos = self._turt.turtle.pos()
+            for i in range(yrange_):
+                self.flowerp(petals, size, curverange, pos=[curpos[0], curpos[1] - radius], colors=colors)
+                curpos = self._turt.turtle.pos()
+
+    def half_flower(self, npetals=5, curverange=10, petalfwd=15, curveangle=9, colors=color_list2):
+        for i in range(npetals // 2):
+            col = colors[i % len(colors)]
+            if isinstance(colors, str):
+                col = colors
+            self._turt.color = col
+            self.petal(curverange, petalfwd, curveangle)
+            self.right(360 / npetals)
+
+
+speed = 100
 turtle.bgcolor('black')
-turtle.tracer(speed, 0)
+turtle.tracer(speed, 1)
 
 
 '''
@@ -257,7 +432,7 @@ yellowgreenblue = ColorScheme(51, [255, 0], [255, 150], [0, 255])
 # Here I create a turtle object named franklin, and give him a color scheme to follow
 # argument 2 is pensize and argument 3 is speed, although this is currently overridden above by the tracer method
 # to give him a color scheme, put the schemename.hexcolors as the first argument
-franklin = Turt(color_list2, 1, 10)
+franklin = Turt(yellowgreenblue.hexcolors, 1, 10)
 
 # I then create a pattern instance, assigning the turtle we want to execute the patterns
 # The second argument is the number of repeats
@@ -272,9 +447,21 @@ Here are some patterns to try.
 '''
 # patterngen1.rainbowcircles(100, 5)
 # patterngen1.many_circles(50, 10)
-patterngen1.cardioid_flower(50, 5)
+# patterngen1.cardioid_flower(50, 5)
 # patterngen1.hypercardioid_thing(50, 5)
 # patterngen1.record_thing()
+# patterngen1.flowerp(100, 1)
+# franklin.heading = 45
+
+# patterngen1.curve(1, 10, 15, 9, 'yellow')       # 102.80, 87.80        10.61,  134.77    hypotenuse /  fwd = angle
+# print(franklin.turtle.position())                                                        # radius   =  fwd * angle
+# print(franklin.turtle.distance(0, 0))                                         #               135     /   15   =   9
+
+# franklin.turtle.dot(5)
+# size, curverange, petalfwd, curveangle
+# patterngen1.half_flower(26)
+# patterngen1.many_flowers(26, 5)
+patterngen1.many_flowers()
 
 
-turtle.exitonclick() # This makes it so that the window doesn't close itself once it's done drawing
+turtle.exitonclick()   # This makes it so that the window doesn't close itself once it's done drawing
